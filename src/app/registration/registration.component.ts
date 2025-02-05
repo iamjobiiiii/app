8ee0556b9f1ce8ea0f3fae0f51b894b3e57cbd72
iamjobiiiii <iamjobiiiiiii@gmail.com>
2025-02-05 21:43:36 +0530

    hehe

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
+
+  registrationform: any = {
+
+    userid: '',
+    password: ''
+  }
 
 }
