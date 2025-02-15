652d8ccbec647586e6169c5d9a2633546b7bf99f
jaseera <jaseera.sa.firecodex.com>
2025-02-15 11:56:04 +0530

    hehhe

--- src/app/publicauth/registration/registration.component.ts
+++ src/app/publicauth/registration/registration.component.ts
@@ -1,7 +1,7 @@
 import { Component} from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, ReactiveFormsModule,FormBuilder,FormControl, Validators } from '@angular/forms';
-import { AuthService } from '../../auth.service';
+
 
 @Component({
   selector: 'app-registration',
@@ -13,30 +13,18 @@ export class RegistrationComponent {
   Registerform: FormGroup;
   errorMessage: string = '';
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+  constructor(private fb: FormBuilder) {
     // Define form controls and validation
     this.Registerform = this.fb.group({
       userid: ['', Validators.required],
       username: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      role: ['',Validators.required]
     });
   }
 
   onsave(): void {
-    if (this.Registerform.valid) {
-      const { userid, username, password } = this.Registerform.value;
-      
-      // Call register service method
-      this.authService.register(userid, username, password, 'user').subscribe({
-        next: (response) => {
-          this.authService.storeToken(response.token);
-          this.router.navigate(['/protected']);  // Redirect to a protected route
-        },
-        error: (err) => {
-          this.errorMessage = err.error.message || 'Registration failed. Please try again.';
-        }
-      });
-    }
+    
   }
 
 }
